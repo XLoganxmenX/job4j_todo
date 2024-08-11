e5d5c4b1aabc52f906c998c02e63b73ad3706d9c
XLoganxmenX <loganxmen97@gmail.com>
2024-08-11 16:26:52 +0900

    add Task model and it repository

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,43 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+    private LocalDateTime created = LocalDateTime.now();
+    private boolean done;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        return id == task.id && done == task.done && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, done);
+    }
+}
